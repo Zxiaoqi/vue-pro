214c4c66d571101c2b662fb1caef0431393efe93
zxiaoqi <mrzeng18229230299@163.com>
2020-02-24 15:22:42 +0800

    小修改

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -9,28 +9,56 @@
                 shape="round"
                 left-icon="search"
                 placeholder="请输入搜索关键词"
-                @search="onSearch">
+                @search="onSearch"
+                @clear="clearVal">
                 <div slot="action" @click="onSearch">搜索</div>
             </van-search>
         </div>
-        <van-panel title="历史记录" v-if="oldValue.length!==0">
-            <div class="searchTitle" v-for="(item,i) in oldValue" :key="i"
-            @click="toDetail(item.id)">{{item.title}}</div>
-        </van-panel>
-        <van-panel title="热门搜索" v-else-if="recomSearch.length!==0">
-            <div class="searchTitle" v-for='(item,i) of recomSearch' :key="i"
-            @click="toDetail(item.id)">{{item.title}}</div>
-        </van-panel>
+        <div class="search-content">
+            <template v-if="!showSearch">
+                <van-panel title="历史记录" v-if="keyword.length!==0">
+                    <!-- <div class="searchTitle" v-for="(item,i) in oldValue" :key="i"
+                    @click="toDetail(item.id)">{{item.title}}</div> -->
+                    <van-row type="flex">
+                        <van-col span="6" v-for="(item,i) in keyword" :key="i">
+                            <van-tag
+                            v-if="item"
+                            closeable
+                            round
+                            size="medium"
+                            @close="close(i)"
+                            @click="getSearch(item)">{{item}}</van-tag>
+                        </van-col>
+                    </van-row>
+                </van-panel>
+                <van-panel title="热门搜索" v-if="recomSearch.length!==0">
+                    <div class="searchTitle" v-for='(item,i) of recomSearch' :key="i"
+                    @click="toDetail(item.id)">{{item.title}}</div>
+                </van-panel>
+            </template>
+
+            <template v-if="showSearch">
+                <Cover :post="item" v-for="(item,i) in oldValue" :key="i"
+                @click="toDetail(item.id)">
+                    <span>{{item.comment_length}}跟帖</span>
+                </Cover>
+            </template>
+        </div>
     </div>
 </template>
 
 
 <script>
-
+import Cover from '../components/CoverList'
 export default {
+    components:{
+        Cover
+    },
     data() {
         return {
             value:'',
+            keyword:[],
+            showSearch:false,
             oldValue:[],
             recomSearch:[],
             pageIndex:0
@@ -41,23 +69,43 @@ export default {
         toDetail(id){
             this.$router.push({path:'/artdetail',query:{id:id}})
         },
+        getSearch(value,pageIndex=1){
+            this.$http.get('/post_search',
+                {params:{
+                    keyword:value,
+                    pageIndex:pageIndex,
+                    pageSize:2
+                }})
+                .then(res=>{
+                // console.log(res);
+                const {data}=res.data
+                this.showSearch=true
+                if(data.length!=0){
+                    this.oldValue=[...this.oldValue,...data]
+                    this.keyword.push(this.value)
+                    localStorage.setItem('keyword',JSON.stringify(this.keyword))
+                }
+            })
+        },
         onSearch(){
             if(this.value.trim().length!=0){
                 this.pageIndex ++
-                this.$http.get('/post_search',
-                {params:{
-                    keyword:this.value,
-                    pageIndex:this.pageIndex,
-                    pageSize:2
-                }}).then(res=>{
-                    // console.log(res);
-                    const {data}=res.data
-                    if(data.length!=0){
-                        this.oldValue=[...this.oldValue,...data]
-                        localStorage.setItem('oldSearch',JSON.stringify(this.oldValue))
-                    }
-                })
+                this.getSearch(this.value,this.pageIndex)
             }
+        },
+        //关闭标签
+        close(i){
+            if(this.keyword.length!==0){
+                this.keyword.splice(i,1)
+                localStorage.setItem('keyword',JSON.stringify(this.keyword))
+            }
+        },
+        clearVal(){
+            this.showSearch=false
+        },
+        //跳转详情
+        toRegister(){
+            this.$router.push('/register')
         },
         //推荐搜索
         recommendSearch(){
@@ -76,9 +124,9 @@ export default {
         }
     },
     created() {
-        const oldSearch=JSON.parse(localStorage.getItem('oldSearch'))
-        if(oldSearch){
-            this.oldValue=oldSearch
+        const keyword=JSON.parse(localStorage.getItem('keyword'))||[]
+        if(keyword[0]){
+            this.keyword=keyword
         }
         this.recommendSearch()
     },
@@ -100,8 +148,10 @@ export default {
         .van-cell__title
             span
                 font-size 3.611vw
+        .van-col
+            text-align center
         div.searchTitle
-            padding 1.667vw 4.444vw
+            padding 1.667vw 1.389vw
             color #666
             font-size 3.611vw
             white-space nowrap
@@ -110,4 +160,6 @@ export default {
             box-shadow 0 0 1px 0 #ccc inset
             &:active
                 filter brightness(130%)
+    .search-content
+        padding 0 2.778vw
 </style>
